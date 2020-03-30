142c0afca830b42bd18e329f9890c75c073f1a60
geofeid <fidakisgeorge@yahoo.com>
2020-03-30 17:59:22 +0300

    Module 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
