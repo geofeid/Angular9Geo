fdc2e8132d9b1fffe77de6578bfe95deb1dd999f
geofeid <fidakisgeorge@yahoo.com>
2020-03-30 20:08:33 +0300

    Module 23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -11,10 +12,12 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
 import { LeaderService } from './services/leader.service';
+import { FeedbackService } from './services/feedback.service';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { baseURL } from './shared/baseurl';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -24,14 +27,18 @@ import { baseURL } from './shared/baseurl';
     DishdetailComponent,
     FooterComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [DishService,LeaderService,
+  providers: [DishService,LeaderService,FeedbackService,
     {provide: 'BaseURL', useValue: baseURL}],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/feedback.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Feedback } from '../shared/feedback';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedbackService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeedbacks(): Observable<Feedback[]> {
+    return this.http.get<Feedback[]>(baseURL + 'feedback');
+}
+
+addFeedbacks(fb: Feedback): Observable<Feedback[]> {
+  return this.http.post<Feedback[]>(baseURL + 'feedback', fb);
+ }
+
+}
